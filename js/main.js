78ad695c26a6c04014ed3550b9375aa4a0364cc3
Marie Otaki <otakimarie@gmail.com>
2019-05-01 17:44:18 -0700

    add a delete button

--- js/main.js
+++ js/main.js
@@ -1,14 +1,21 @@
 function getNewNote(){
   return '<div class="note" draggable="true">' +
          '<input type="text">' +
+         '<input class="delete-button" type="button" value="delete">' +
          '</div>';
 }
 
-$(document).ready(function(){
-  $('#add-button').on('click', function(){
-    var $note = $(getNewNote());
-    $note.draggable();
-    $('#sticky-note-container').append($note);
+$(function(){
+  $(document).ready(function(){
+    $('#add-button').on('click', function(){
+      var $note = $(getNewNote());
+      $note.draggable();
+      $note.children(".delete-button").on('click', function(){
+        $(this).parents('.note').remove();
+      });
+      $('#sticky-note-container').append($note);
+    });
   });
 });
+
 
