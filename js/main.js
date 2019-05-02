5e33d4953f27482222fa210e898fc3cb0f281549
Marie Otaki <otakimarie@gmail.com>
2019-05-01 17:58:54 -0700

    add buttons that can change color of themselves

--- js/main.js
+++ js/main.js
@@ -2,17 +2,30 @@ function getNewNote(){
   return '<div class="note" draggable="true">' +
          '<input type="text">' +
          '<input class="delete-button" type="button" value="delete">' +
+         '<input data-color="#e0020" class="color-button" type="button" value="red">'+
+         '<input data-color="#e0ff20" class="color-button" type="button" value="yellow">'+
          '</div>';
 }
 
 $(function(){
   $(document).ready(function(){
-    $('#add-button').on('click', function(){
-      var $note = $(getNewNote());
+    function appendFunctions($note) {
       $note.draggable();
+
       $note.children(".delete-button").on('click', function(){
         $(this).parents('.note').remove();
       });
+
+      $note.children(".color-button").on('click', function(){
+        const color = $(this).data('color');
+        $(this).parents('.note').css('background-color', color);
+      });
+    }
+
+    $('#add-button').on('click', function(){
+      var $note = $(getNewNote());
+      appendFunctions($note);
+
       $('#sticky-note-container').append($note);
     });
   });
