476c2da897010691324c92c79e5106e8004e64bb
Marie Otaki <otakimarie@gmail.com>
2019-05-02 17:42:41 -0700

    first commit

--- js/main.js
+++ js/main.js
@@ -1,36 +1,102 @@
-function getNewNote(){
-  return '<div class="note" draggable="true">' +
-         '<textarea class="expanding"></textarea>' +
-         '<input class="delete-button" type="button" value="delete">' +
-         '<input data-color="#e0020" class="color-button" type="button" value="red">'+
-         '<input data-color="#e0ff20" class="color-button" type="button" value="yellow">'+
-         '</div>';
-}
+(function($)
+{
+    /**
+     * Auto-growing textareas; technique ripped from Facebook
+     *
+     * https://github.com/jaz303/jquery-grab-bag/tree/master/javascripts/jquery.autogrow-textarea.js
+     */
+    $.fn.autogrow = function(options)
+    {
+        return this.filter('textarea').each(function()
+        {
+            var self         = this;
+            var $self        = $(self);
+            var minHeight    = $self.height();
+            var noFlickerPad = $self.hasClass('autogrow-short') ? 0 : parseInt($self.css('lineHeight')) || 0;
 
-$(function(){
-  $(document).ready(function(){
-    function appendFunctions($note) {
-      $note.draggable();
+            var shadow = $('<div></div>').css({
+                position:    'absolute',
+                top:         -10000,
+                left:        -10000,
+                width:       $self.width(),
+                fontSize:    $self.css('fontSize'),
+                fontFamily:  $self.css('fontFamily'),
+                fontWeight:  $self.css('fontWeight'),
+                lineHeight:  $self.css('lineHeight'),
+                resize:      'none',
+                'word-wrap': 'break-word'
+            }).appendTo(document.body);
 
-      $note.resizable();
+            var update = function(event)
+            {
+                var times = function(string, number)
+                {
+                    for (var i=0, r=''; i<number; i++) r += string;
+                    return r;
+                };
 
-      $note.children(".delete-button").on('click', function(){
-        $(this).parents('.note').remove();
-      });
+                var val = self.value.replace(/</g, '&lt;')
+                                    .replace(/>/g, '&gt;')
+                                    .replace(/&/g, '&amp;')
+                                    .replace(/\n$/, '<br/>&nbsp;')
+                                    .replace(/\n/g, '<br/>')
+                                    .replace(/ {2,}/g, function(space){ return times('&nbsp;', space.length - 1) + ' ' });
 
-      $note.children(".color-button").on('click', function(){
-        const color = $(this).data('color');
-        $(this).parents('.note').css('background-color', color);
-      });
-    }
+                // Did enter get pressed?  Resize in this keydown event so that the flicker doesn't occur.
+                if (event && event.data && event.data.event === 'keydown' && event.keyCode === 13) {
+                    val += '<br />';
+                }
 
-    $('#add-button').on('click', function(){
-      var $note = $(getNewNote());
-      appendFunctions($note);
+                shadow.css('width', $self.width());
+                shadow.html(val + (noFlickerPad === 0 ? '...' : '')); // Append '...' to resize pre-emptively.
+                $self.height(Math.max(shadow.height() + noFlickerPad, minHeight));
+            }
 
-      $('#sticky-note-container').append($note);
+            $self.change(update).keyup(update).keydown({event:'keydown'},update);
+            $(window).resize(update);
+
+            update();
+        });
+    };
+})(jQuery);
+
+
+var noteTemp =  '<div class="note">'
+        + '<a href="javascript:;" class="button remove">X</a>'
+        +   '<div class="note_cnt">'
+        +   '<textarea class="title" placeholder="Enter note title"></textarea>'
+        +     '<textarea class="cnt" placeholder="Enter note description here"></textarea>'
+        + '</div> '
+        +'</div>';
+
+var noteZindex = 1;
+function deleteNote(){
+    $(this).parent('.note').hide("puff",{ percent: 133}, 250);
+};
+
+function newNote() {
+  $(noteTemp).hide().appendTo("#board").show("fade", 300).draggable().on('dragstart',
+    function(){
+       $(this).zIndex(++noteZindex);
     });
-  });
+ 
+  $('.remove').click(deleteNote);
+  $('textarea').autogrow();
+  
+  $('.note')
+  return false; 
+};
+
+
+
+$(document).ready(function() {
+    
+    $("#board").height($(document).height());
+    
+    $("#add_new").click(newNote);
+    
+    $('.remove').click(deleteNote);
+    newNote();
+    
+    return false;
 });
-
-
