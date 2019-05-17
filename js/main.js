b686731fae7e9cc4dcea8f4ac2c5ac68b815501e
Marie Otaki <otakimarie@gmail.com>
2019-05-17 11:07:45 -0700

    add delete button and textarea

--- js/main.js
+++ js/main.js
@@ -1,37 +1,50 @@
-(function(){
     'use strict';
+
+    window.onload = function(){
 
     const addBtn = document.getElementById('add');
     const board = document.getElementById('board');
-    let cardNum = 1;
+    let cardNum = 0;
+
+    //create the 1st card
+    addCard();
+
+    addBtn.addEventListener('click', ()=>{
+        addCard();
+    });
 
     function createCard(){
+        cardNum++;
+
+        //create delete button
+        const deleteBtn = document.createElement('button');
+        deleteBtn.innerText = 'X';
+        deleteBtn.classList.add('delete');
+
+        deleteBtn.addEventListener('click', (event)=>{
+            const targetId = event.target.parentElement.id;
+            const targetCard = document.getElementById(targetId);
+            targetCard.parentNode.removeChild(targetCard);
+        });
+
+        //create card
         let card = document.createElement('div');
+        card.id = cardNum;
         card.classList.add('card');
 
-        let cardContent = '<button class="delete">X</button><textarea class="card_textarea"></textarea>';
+        card.appendChild(deleteBtn);
 
-        card.innerHTML = cardContent;
+        const cardTextarea = document.createElement('textarea');
+        cardTextarea.classList.add('card_textarea');
+        card.appendChild(cardTextarea);
+
+        board.appendChild(card);
+        return card;
+    }
+
+    function addCard(){
+        const card = createCard();
         board.appendChild(card);
     }
 
-    addBtn.addEventListener('click', function(){
-        createCard();
-    });
-
-    let deleteBtn = document.getElementsByClassName('delete');
-
-    console.log(deleteBtn);
-
-    for(let i = 0; i < deleteBtn.length; i++){
-        let btn =  deleteBtn[i];
-
-        let card = document.getElementsByClassName('card')[0];
-
-        btn.addEventListener('click', function(){
-            console.log(this.parentNode.parentNode.removeChild(card));
-        });
-    }
-
-    
-})();
+};
